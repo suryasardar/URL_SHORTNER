4a8ce0b1fdf89dec92ebd5983881389e55108197
suryasardar <suryaprakashchary515@gmail.com>
2024-11-26 23:29:22 +0530

    added parse true in mongo connection

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
  
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true })
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
